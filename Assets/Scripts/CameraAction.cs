94e64334f0918b600000021678f5c4858e1210e9
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-06-04 19:15:34 -0300

    Alterado nome da cena para Main, criado o chão com o material chao, criado o empty GameObject Player e feito a camera seguir o mouse do jogador com o script CameraAction

---
+++ Assets/Scripts/CameraAction.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraAction : MonoBehaviour
+{
+    void Update()
+    {
+        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+    }
+}
