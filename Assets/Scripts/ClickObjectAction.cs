34f2ca54b8ed9fca36c0cbd29788cfe76292d07a
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-06-04 19:31:12 -0300

    Criado os scripts ClickObjectAction e PlayerAction, feito no PlayerAction o sistema de pontos do jogo e feito o ClickObject ao ser clicado aumentar os pontos

---
+++ Assets/Scripts/ClickObjectAction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ClickObjectAction : MonoBehaviour
+{
+    public PlayerAction player;
+    public int scorePointsOnClick = 1;
+
+    void OnMouseDown()
+    {
+        player.score += scorePointsOnClick;
+    }
+}
---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerAction : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
