7755fd57e47cf108c8220a69a4b687083f88a4a6
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-06-18 14:51:30 -0300

    Added Gyroscope code in CameraAction and added Camera Image to a RawImage inside the game

--- Assets/Scripts/CameraAction.cs
+++ Assets/Scripts/CameraAction.cs
@@ -2,10 +2,22 @@
 
 public class CameraAction : MonoBehaviour
 {
+    private void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
     void Update()
     {
-        float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
-        float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
-        transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+        if(SystemInfo.supportsGyroscope)
+        {
+            transform.Rotate (-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, -Input.gyro.rotationRateUnbiased.z);
+        }
+        else
+        {
+            float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+            float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+            transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+        }
     }
 }
---
+++ Assets/Scripts/CameraViewTexture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraViewTexture : MonoBehaviour
+{
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        WebCamDevice camera = new WebCamDevice();
+        foreach (WebCamDevice cam in devices)
+        {
+            if (!cam.isFrontFacing)
+            {
+                camera = cam;
+                break;
+            }
+        }
+
+        if(String.IsNullOrWhiteSpace(camera.name))
+        {
+            camera = WebCamTexture.devices[WebCamTexture.devices.Length - 1];
+        }
+
+        var rawImage = GetComponent<RawImage>();
+        //yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+        WebCamTexture webcamTexture = new WebCamTexture(camera.name);
+        rawImage.texture = webcamTexture;
+        rawImage.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
