7de7c9d45fa7ad4266a0b50ace9eec6d339d45c8
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-10-18 22:42:38 -0300

    Added Random Scenary

---
+++ Assets/GameFiles/Scripts/SpawnObjects.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public GameObject[] objects;
+    public int min;
+    public int max;
+    public float minDistanceFromSpawn = 10;
+    public float maxDistanceFromSpawn = 30;
+    public float spawnInterval;
+
+    GameObject randomObject;
+    int maxcount;
+    int count = 0;
+
+    void Start()
+    {
+        maxcount = Random.Range(min, max);
+        if(spawnInterval == 0)
+        {
+            for(int i = 0; i < maxcount; i++)
+            {
+                Spawn();
+            }
+        }
+        else
+        {
+            InvokeRepeating("Spawn", 0, spawnInterval);
+        }
+    }
+
+    void Update()
+    {
+        if(count > maxcount)
+        {
+            CancelInvoke();
+        }    
+    }
+
+    void Spawn()
+    {
+        int index = Random.Range(0, objects.Length);
+        randomObject = Instantiate(objects[index]);
+
+        randomObject.transform.parent = transform;
+
+        randomObject.transform.localPosition = new Vector3(
+            Random.Range((transform.position.x + minDistanceFromSpawn) - maxDistanceFromSpawn, transform.position.x + maxDistanceFromSpawn),
+            0,
+            Random.Range((transform.position.z + minDistanceFromSpawn) - maxDistanceFromSpawn, transform.position.z + maxDistanceFromSpawn)
+        );
+        count++;
+    }
+}
