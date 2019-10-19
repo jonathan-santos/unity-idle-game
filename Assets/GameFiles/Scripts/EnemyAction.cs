73877b8166b803e47e0d7c487f2cb453e83eea5e
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-10-18 23:11:43 -0300

    Created script EnemyAction, Tag clickObject, enemiesSpawner and made enemies follow clickObject

---
+++ Assets/GameFiles/Scripts/EnemyAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAction : MonoBehaviour
+{
+    NavMeshAgent agent;
+    GameObject clickObject;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        clickObject = GameObject.FindGameObjectsWithTag("clickObject")[0];
+        agent.destination = clickObject.transform.position;
+    }
+}
