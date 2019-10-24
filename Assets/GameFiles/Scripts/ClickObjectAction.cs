4a27775656cf5041c0531e16738bfcb5ea39a69f
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-10-24 11:11:29 -0300

    Made that ClickObject is now used with bullets and not clicks

--- Assets/GameFiles/Scripts/ClickObjectAction.cs
+++ Assets/GameFiles/Scripts/ClickObjectAction.cs
@@ -5,14 +5,14 @@ public class ClickObjectAction : MonoBehaviour
     public PlayerAction player;
     public int scorePointsOnClick = 1;
 
-    void OnMouseDown()
-    {
-        player.score += scorePointsOnClick;
-    }
-
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "enemy")
+        if(collision.gameObject.tag == "bullet")
+        {
+            player.score += scorePointsOnClick;
+            Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.tag == "enemy")
         {
             player.score -= 10;
             if (player.score < 0)
