a5569d6d4099a1f08d0d9674a94520d7378f115b
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2019-11-12 13:11:43 -0300

    Created ExplosionPrefab and made Enemy create one when killed

--- Assets/GameFiles/Scripts/EnemyAction.cs
+++ Assets/GameFiles/Scripts/EnemyAction.cs
@@ -3,13 +3,19 @@ using UnityEngine.AI;
 
 public class EnemyAction : MonoBehaviour
 {
-    NavMeshAgent agent;
-    GameObject clickObject;
+    public GameObject explosion;
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        clickObject = GameObject.FindGameObjectsWithTag("clickObject")[0];
+        var clickObject = GameObject.FindGameObjectWithTag("clickObject");
+        var agent = GetComponent<NavMeshAgent>();
         agent.destination = clickObject.transform.position;
+        transform.LookAt(clickObject.transform.position, Vector3.up);
+    }
+
+    private void OnDestroy()
+    {
+        var newExplosion = Instantiate(explosion);
+        newExplosion.transform.position = transform.position;
     }
 }
---
+++ Assets/GameFiles/Scripts/ExplosionAction.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ExplosionAction : MonoBehaviour
+{
+    void Start()
+    {
+        var explosion = GetComponent<ParticleSystem>();
+        var main = explosion.main;
+        main.loop = false;
+        explosion.Play();
+        Destroy(gameObject, main.duration);
+    }
+}
